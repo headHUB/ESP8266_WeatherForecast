a4d64f822b0214fdeaaca2132ef244996042f468
hidenorly <twitte.harold@gmail.com>
2017-01-14 22:51:45 +0900

    Add LCD Manager with PIR sensor detection or GPI0 BTN

--- ESP8266_IoT/ESP8266_IoT.ino
+++ ESP8266_IoT/ESP8266_IoT.ino
@@ -14,6 +14,10 @@
  limitations under the License.
 */
 
+extern "C" {
+#include "user_interface.h"
+}
+
 #include "base.h"
 #include "config.h"
 
@@ -64,6 +68,34 @@ void onWiFiClientConnected(){
 // Initialize TFT LCD as global
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
+class LCDManager
+{
+  public:
+    static bool getEnabled(void){ return mEnableStatus; }
+    static void setEnableLCD(bool bEnable) {
+      if( mEnableStatus!=bEnable ) {
+        if( bEnable ){
+          pinMode(TFT_RST, OUTPUT);
+          digitalWrite(TFT_RST, LOW);
+          delay(10);
+          digitalWrite(TFT_RST, HIGH);
+        
+          tft.begin();
+          tft.setRotation(3);
+          tft.fillScreen(ILI9341_BLACK);
+        } else {
+          pinMode(TFT_RST, OUTPUT);
+          digitalWrite(TFT_RST, LOW);
+        }
+        mEnableStatus = bEnable;
+      }
+    }
+
+  protected:
+    static bool mEnableStatus;
+};
+
+bool LCDManager::mEnableStatus=false;
 
 class Poller:public LooperThreadTicker
 {
@@ -71,17 +103,9 @@ class Poller:public LooperThreadTicker
     Poller(int dutyMSec=0):LooperThreadTicker(NULL, NULL, dutyMSec)
     {
     }
-    virtual void doCallback(void)
+    void updateLCD(Weather& weather)
     {
-      char s[30];
-      time_t n = now();
-      sprintf(s, "%04d-%02d-%02d %02d:%02d:%02d", year(n), month(n), day(n), hour(n), minute(n), second(n));
-    
-      DEBUG_PRINT("UTC : ");
-      DEBUG_PRINTLN(s);
-
-      static Weather weather;
-      if( weather.getLatestWeather() ) {
+      if( LCDManager::getEnabled() ){
         tft.setRotation(3);
         tft.fillScreen(ILI9341_BLACK);
         tft.setCursor(0, 0);
@@ -93,12 +117,33 @@ class Poller:public LooperThreadTicker
 //      tft.println(weather.icon);
 
         tft.setTextSize(4);
+        char s[30];
+        time_t n = now();
         time_t t = localtime(n, 9);
         sprintf(s, "%02d/%02d %02d:%02d", month(t), day(t), hour(t), minute(t));
         tft.println(s);
       }
+    }
+
+    virtual void doCallback(void)
+    {
+      char s[30];
+      time_t n = now();
+      sprintf(s, "%04d-%02d-%02d %02d:%02d:%02d", year(n), month(n), day(n), hour(n), minute(n), second(n));
+    
+      DEBUG_PRINT("UTC : ");
+      DEBUG_PRINTLN(s);
+
+      static Weather weather;
+      static bool lastLCDstatus = false;
+      bool currentLCDStatus = LCDManager::getEnabled();
+      if( weather.getLatestWeather() || (currentLCDStatus && lastLCDstatus!=currentLCDStatus) ) {
+        updateLCD(weather);
+      }
+      lastLCDstatus = currentLCDStatus;
   }
 };
+
 
 
 // --- General setup() function
@@ -120,19 +165,34 @@ void setup() {
   }
 
   // Initialize LCD
-  pinMode(TFT_RST, OUTPUT);
-  digitalWrite(TFT_RST, LOW);
-  delay(10);
-  digitalWrite(TFT_RST, HIGH);
+  LCDManager::setEnableLCD(true);
+}
 
-  tft.begin();
-  tft.setRotation(3);
-  tft.fillScreen(ILI9341_BLACK);
+#define HUMAN_DETECTION_THRESHOLD 100
+#define LCD_ON_HISTERESES 10000
+
+void handleLcdBacklight(void)
+{
+  static unsigned long lastMeasured = 0;
+  static unsigned long lastOn = 0;
+  unsigned long n = millis();
+  if( (n - lastMeasured) < 100 ) return; // within 100msec barrier
+
+  int adcVal = system_adc_read(); // PIR output is connected to TOUT
+  DEBUG_PRINTLN("ADC : "+String(adcVal));
+  if( (adcVal > HUMAN_DETECTION_THRESHOLD) || (digitalRead(MODE_PIN) == 0) ) {
+    lastOn = n;
+    LCDManager::setEnableLCD(true);
+  } else if( (n-lastOn) > LCD_ON_HISTERESES ) {
+    LCDManager::setEnableLCD(false);
+  }
+  lastMeasured = n;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   handleWiFiClientStatus();
   handleWebServer();
+  handleLcdBacklight();
   g_LooperThreadManager.handleLooperThread();
 }
